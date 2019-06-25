c1f02b29c4c7f25274c7584382ae4956a03f15ac
dave8git <dave8git@gmail.com>
2019-06-26 00:39:28 +0200

    fix gallery

--- js/script.js
+++ js/script.js
@@ -14,13 +14,6 @@ var flkty = new Flickity( elem, {
   wrapAround: true
 });
 
-// element argument can be a selector string
-//   for an individual element
-var flkty = new Flickity( '.main-carousel', {
-  // options
-});
-var flkty = new Flickity('.main-carousel');
-
 var progressBar = document.querySelector('.progress-bar')
 
 flkty.on( 'scroll', function( progress ) {
