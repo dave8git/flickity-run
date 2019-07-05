e2356432d72aea520b99a4c9da118c03fbfbcd29
unknown <dave8git@gmail.com>
2019-07-05 12:12:43 -0700

    fix library link and progressbar

--- js/script.js
+++ js/script.js
@@ -29,10 +29,10 @@ var flkty = new Flickity( elem, {
 
 var progressBar = document.querySelector('.progress-bar');
 
-flkty.on( 'scroll', function( progress ) {
-  progress = Math.max( 0, Math.min( 1, progress ) );
-  progressBar.style.width = progress * 100 + '%';
-});
+// flkty.on( 'scroll', function( progress ) {
+//   progress = Math.max( 0, Math.min( 1, progress ) );
+//   progressBar.style.width = progress * 100 + '%';
+// });
 
 
 
