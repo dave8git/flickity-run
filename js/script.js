78ceab1d79cf981eea437c9e37157b269e3d9d5c
davepol <davepol@gmail.com>
2019-07-08 00:10:52 +0200

    fix restart button

--- js/script.js
+++ js/script.js
@@ -1,17 +1,18 @@
 var buttons = document.querySelectorAll('.button');
+
 var carouselOwl = ''; 
 
 var carouselTemplate = document.getElementById('template').innerHTML;
 
 
 for(var i = 0; i < tablicaSow.length; i++){
-  console.log(tablicaSow[i]);
   carouselOwl += Mustache.render(carouselTemplate, tablicaSow[i]);
 }
 
 document.querySelector('.main-carousel').innerHTML = carouselOwl;
 
-console.log(carouselOwl); 
+
+
 
 for (i = 0; i < buttons.length; i++) { 
 buttons[i].addEventListener( 'click', function( event ) {
@@ -19,13 +20,22 @@ buttons[i].addEventListener( 'click', function( event ) {
   flkty.selectCell( selector );
 });
 };
+
 var elem = document.querySelector('.main-carousel');
 var flkty = new Flickity( elem, {
   // options
   cellAlign: 'left',
-  contain: true, 
-  wrapAround: true
+  contain: true,
+  imagesLoaded: true,
+  percentPosition: false,
+  hash: true,
 });
+
+var buttonRestart = document.querySelector('.button-restart');
+buttonRestart.addEventListener( 'click', function( e ) {
+  flkty.select( 0 );
+});
+
 
 var progressBar = document.querySelector('.progress-bar');
 
